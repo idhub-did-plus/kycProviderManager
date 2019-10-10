874deac3cdd56b3d3627ce44f68ea3dbaa2f4115
laixiaoyin <1012551253@qq.com>
2019-10-10 11:16:50 +0800

    remove login page

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,10 @@ import history from "./history"
 import management from "./management"
 import processing from "./processing"
 import newPage from "./newPage"
-import Login from "./login"
 
 export default new Router({
   routes: [
-    // {path:"/",redirect:"unReceivedList"},
-    {path:"/",redirect:"login"},
-    processingDetail,unreceived,management,history,processing,newPage,Login
+    {path:"/",redirect:"unReceivedList"},
+    processingDetail,unreceived,management,history,processing,newPage
   ]
 })
--- src/router/login.js
+++
@@ -1,5 +0,0 @@
-export default {
-    name:"login",
-    path:"/login",
-    component:()=>import("@/views/login.vue")
-}
